3cccfad8ed818fb2dc6b03e16aaa05a8045f0e9b
2026-DEV2-026 <ID+username@users.noreply.github.com>
2026-01-31 18:52:33 +0530

    refactor : GameEvaluator calculateWinner

--- app/src/main/java/com/example/tictactoekata/domain/GameEvaluator.kt
+++ app/src/main/java/com/example/tictactoekata/domain/GameEvaluator.kt
@@ -7,41 +7,16 @@ class GameEvaluator @Inject constructor() {
     companion object {
         val WINNING_ROWS = listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8))
         val WINNING_COLUMNS = listOf(listOf(0, 3, 6), listOf(1, 4, 7), listOf(2, 5, 8))
-
         val WINNING_DIAGONALS = listOf(listOf(0, 4, 8), listOf(2, 4, 6))
+
+        private val WINNING_PATTERNS = WINNING_ROWS + WINNING_COLUMNS + WINNING_DIAGONALS
     }
 
     fun calculateWinner(board: Board): Player? {
-        for (row in WINNING_ROWS) {
-            val (first, second, third) = row
-            val cellFirst = board.cells[first]
-            if (cellFirst.player != null &&
-                cellFirst.player == board.cells[second].player &&
-                cellFirst.player == board.cells[third].player
-            ) {
-                return cellFirst.player
+        return WINNING_PATTERNS.firstNotNullOfOrNull { (first, second, third) ->
+            board.cells[first].player?.takeIf { player ->
+                player == board.cells[second].player && player == board.cells[third].player
             }
         }
-        for (col in WINNING_COLUMNS) {
-            val (first, second, third) = col
-            val cellFirst = board.cells[first]
-            if (cellFirst.player != null &&
-                cellFirst.player == board.cells[second].player &&
-                cellFirst.player == board.cells[third].player
-            ) {
-                return cellFirst.player
-            }
-        }
-
-        for(diagonal in WINNING_DIAGONALS){
-            val (first,second,third) = diagonal
-            val cellFirst = board.cells[first]
-            if(cellFirst.player != null &&
-                cellFirst.player == board.cells[second].player &&
-                cellFirst.player == board.cells[third].player){
-                return cellFirst.player
-            }
-        }
-        return null
     }
 }
