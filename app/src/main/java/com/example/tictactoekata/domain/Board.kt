8a515cd013b771a5c7df582b90e04dd56d2fcf70
2026-DEV2-026 <ID+username@users.noreply.github.com>
2026-01-31 16:17:34 +0530

    refactor : Board - removed magic number

--- app/src/main/java/com/example/tictactoekata/domain/Board.kt
+++ app/src/main/java/com/example/tictactoekata/domain/Board.kt
@@ -1,9 +1,12 @@
 package com.example.tictactoekata.domain
 
+const val BOARD_SIZE = 3
+const val TOTAL_CELLS = BOARD_SIZE * BOARD_SIZE
 data class Board(
-    val cells: List<Cell> = List(9) { Cell() }
+    val cells: List<Cell> = List(TOTAL_CELLS) { Cell() }
 ) {
     fun get(index: Int): Cell = cells[index]
+
     fun play(index: Int, player: Player): Board {
         val newCells = cells.toMutableList()
         newCells[index] = Cell(player)
