9775b26f7b34d48051909a6f30b28461ad757917
2026-DEV2-026 <ID+username@users.noreply.github.com>
2026-01-31 18:17:42 +0530

    test : GameEvaluatorTest calculates winner on all rows

--- app/src/main/java/com/example/tictactoekata/domain/GameEvaluator.kt
+++ app/src/main/java/com/example/tictactoekata/domain/GameEvaluator.kt
@@ -3,7 +3,23 @@ package com.example.tictactoekata.domain
 import javax.inject.Inject
 
 class GameEvaluator @Inject constructor() {
-    fun calculateWinner(board : Board): Player? {
+
+    companion object {
+        val WINNING_ROWS = listOf(
+            listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8)
+        )
+    }
+
+    fun calculateWinner(board: Board): Player? {
+        for(row in WINNING_ROWS){
+            val (first, second, third) = row
+            val cellFirst = board.cells[first]
+            if(cellFirst.player != null &&
+                cellFirst.player == board.cells[second].player &&
+                cellFirst.player == board.cells[third].player){
+                return cellFirst.player
+            }
+        }
         return null
     }
 }
--- app/src/test/java/com/example/tictactoekata/domain/GameEvaluatorTest.kt
+++ app/src/test/java/com/example/tictactoekata/domain/GameEvaluatorTest.kt
@@ -1,13 +1,33 @@
 package com.example.tictactoekata.domain
 
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 import kotlin.test.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertNull
 
 class GameEvaluatorTest {
+
+    companion object{
+        @JvmStatic
+        fun providingRows() : List<List<Int>> {
+            return GameEvaluator.WINNING_ROWS
+        }
+    }
     private val evaluator = GameEvaluator()
 
     @Test
     fun `no winner on empty board`(){
         assertNull(evaluator.calculateWinner(Board()))
+    }
+
+    @ParameterizedTest(name = "X wins on row : {0}")
+    @MethodSource("providingRows")
+    fun `calculates winner on all rows`(winningIndices : List<Int>){
+        val board = winningIndices.fold(Board()) { currentBoard, index ->
+            currentBoard.play(index, Player.X)
+        }
+
+        assertEquals(Player.X,evaluator.calculateWinner(board))
     }
 }
